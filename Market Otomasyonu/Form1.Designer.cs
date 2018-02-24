23a4703814e79106ee4dbca6a31255cb9d98c3ae
bayramgeldi <bayramgeldi.bayriyev@gmail.com>
2018-02-24 19:38:03 +0300

    Yöneticiye ve market sahibine görsel eklendi
    Yöneticiye ve market sahibine görsel eklendi

--- Market Otomasyonu/Form1.Designer.cs
+++ Market Otomasyonu/Form1.Designer.cs
@@ -35,22 +35,30 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Info;
-            this.button1.Location = new System.Drawing.Point(12, 68);
+            this.button1.BackgroundImage = global::Market_Otomasyonu.Properties.Resources.admin;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(12, 29);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 42);
+            this.button1.Size = new System.Drawing.Size(120, 120);
             this.button1.TabIndex = 0;
             this.button1.Text = "Yönetici";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.Info;
-            this.button2.Location = new System.Drawing.Point(202, 68);
+            this.button2.BackgroundImage = global::Market_Otomasyonu.Properties.Resources.user;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(193, 29);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(111, 42);
+            this.button2.Size = new System.Drawing.Size(120, 120);
             this.button2.TabIndex = 1;
             this.button2.Text = "Market Sah.";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
